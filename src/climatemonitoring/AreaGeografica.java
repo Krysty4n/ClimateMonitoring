823f0ed679d4ada2fa21e013e66eb943108a4e83
t-greta <gretafly00@gmail.com>
2023-04-05 11:42:46 +0200

    codice

--- src/climatemonitoring/AreaGeografica.java
+++ src/climatemonitoring/AreaGeografica.java
@@ -8,6 +8,42 @@ package climatemonitoring;
  *
  * @author greta
  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class AreaGeografica {
-    
-}
+    //campi
+    private String area;
+    private String stato;
+    private String coord;
+
+    //costruttori
+
+    public AreaGeografica(String area, String stato, String coord) {
+        this.area = area;
+        this.stato = stato;
+        this.coord = coord;
+    }
+
+    //metodi 
+
+    public String cercaAreaGeografica(String area, String stato, String coord) {
+            String line;
+            try{
+                BufferedReader br = new BufferedReader(new FileReader("../data/ClimateMonitoring.dati.csv"));
+                while((line = br.readLine())!= null){
+                    String[] splitLine = line.split(";");
+                    if(splitLine[1].toLowerCase().contains(area.toLowerCase()) || splitLine[5].contains(coord)) {
+                        stato = splitLine[4];
+                    }
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return stato;
+        }
+        
+        
+    }
+
--- src/climatemonitoring/RegistrazioneCentro.java
+++ src/climatemonitoring/RegistrazioneCentro.java
@@ -8,6 +8,31 @@ package climatemonitoring;
  *
  * @author greta
  */
+import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
 public class RegistrazioneCentro {
-    
+
+    private String nome;
+    private String indirizzo;
+    private List<String> aree;
+
+    public RegistrazioneCentro(String nome, String indirizzo, List<String> aree) {
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.aree = aree;
+    }
+
+
+    public static void registraCentroAree(String nome, String indirizzo, List<String> aree) {
+        try {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("..data/OperatoriRegistrati.dati"));
+            bw.write(nome + ";" + indirizzo + ";" + aree);
+        bw.close();
+        } catch(IOException e) {
+            e.printStackTrace();  
+        }
+    }
 }
